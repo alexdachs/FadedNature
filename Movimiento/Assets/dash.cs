1cd40ccea4d8cf6435c478064d5ada6347efe1d6
david-ctrl <56065560+david-ctrl@users.noreply.github.com>
2020-02-10 12:43:02 +0100

    Movimiento
    Script de movimiento y salto

---
+++ Movimiento/Assets/dash.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dash : MonoBehaviour
+{
+
+    public Rigidbody2D rigidBody2D;
+    public float MaxSpeed = 15f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            float Xdireccion = Input.GetAxis("Horizontal");
+            rigidBody2D.velocity = new Vector2(Xdireccion * MaxSpeed, rigidBody2D.velocity.y);
+
+
+        }
+
+    }
+}
+
+    
+
