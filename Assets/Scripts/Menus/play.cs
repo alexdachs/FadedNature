d5e5d22c3056a9614bed4fe93be0e51a97277d4f
Joanmagri <joanmagri1101@gmail.com>
2020-03-02 14:13:51 +0100

    Menus, falta el de opciones

---
+++ Assets/Scripts/Menus/play.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class play : MonoBehaviour
+{
+    public Button _pButon, _oButton, _cButton, _eButton ;
+    private void Start()
+    {
+        _pButon.onClick.AddListener(StartPlay);
+        _oButton.onClick.AddListener(StartOptions);
+        _cButton.onClick.AddListener(StartCredits);
+        _eButton.onClick.AddListener(Exit);
+
+    }
+    void StartPlay()
+    {
+        SceneManager.LoadScene("Enemy_and_ui");
+    }
+    void StartOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    void StartCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    void Exit()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/goBack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class goBack : MonoBehaviour
+{
+    public Button _BackButon;
+    void Start()
+    {
+        _BackButon.onClick.AddListener(GoBack);
+    }
+
+    void GoBack()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
