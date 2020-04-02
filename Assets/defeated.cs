d6b264f6386b80d826a433e200c360ff931499a1
Joanmagri <joanmagri1101@gmail.com>
2020-04-02 13:27:54 +0200

    defeated/tobecontinued

---
+++ Assets/defeated.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class defeated : MonoBehaviour
+{
+public void BackToMenu ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Restart ()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
