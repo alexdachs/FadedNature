e87b79b0efa5c2c200ec96245518b9314219bd5f
Joanmagri <joanmagri1101@gmail.com>
2020-04-03 03:25:40 +0200

    change de musica

--- Assets/Scripts/AudioSource.cs
+++ Assets/Scripts/AudioSource.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioSource : MonoBehaviour
 {
@@ -13,5 +14,7 @@ public class AudioSource : MonoBehaviour
             DontDestroyOnLoad(this);
         }
         
+        
     }
+
 }
