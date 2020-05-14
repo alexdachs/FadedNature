8eef4e036b81be0f2ce11ea23e6eb9931ad76564
Joanmagri <56065500+Joanmagri@users.noreply.github.com>
2020-05-14 18:18:53 +0200

    camara+muros lvl2

--- Assets/Cam.cs
+++ Assets/Cam.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
+    public Transform Start, End;
     public GameObject follow;
     public int maxpos;
    
@@ -11,8 +12,17 @@ public class Cam : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
         float posx = follow.transform.position.x;
 
         transform.position = new Vector3(Mathf.Clamp(posx, 0, maxpos), transform.position.y, transform.position.z);
+        if (transform.position.x <= Start.position.x)
+        {
+            transform.position = new Vector3(Start.position.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x >= End.position.x)
+        {
+            transform.position = new Vector3(End.position.x, transform.position.y, transform.position.z);
+        }
     }
 }
