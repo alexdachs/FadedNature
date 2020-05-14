0f0d372c9974d84c8e743efc2681e3062f7757cd
alexdachs <56065187+alexdachs@users.noreply.github.com>
2020-05-14 03:17:46 +0200

    checkpoints

---
+++ Assets/Scripts/Checkpoints/PlayerPos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+
+    void Start() {
+        gm = GameObject.FinsGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+    }
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
