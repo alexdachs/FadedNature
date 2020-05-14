cda9dec602ebeb56300905b1b677efa31a215461
david-ctrl <56065560+david-ctrl@users.noreply.github.com>
2020-05-14 17:01:29 +0200

    Portal_Lvl2
    Portal_Lvl2

---
+++ Assets/Scripts/Portal/Portal_lvl2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal_lvl2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Player"))
+        {
+            SceneManager.LoadScene("ToBeContinued");
+        }
+    }
+}
+
