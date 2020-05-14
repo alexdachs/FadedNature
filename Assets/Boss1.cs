52f9082e77fb3a88f2afdc48aa2209e150e25a7d
david-ctrl <56065560+david-ctrl@users.noreply.github.com>
2020-05-14 22:07:55 +0200

    Boss_vida
    Boss_vida

--- Assets/Boss1.cs
+++ Assets/Boss1.cs
@@ -2,34 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class muerte : MonoBehaviour
+public class Boss1 : MonoBehaviour
 {
-    private int initHP = 100;
+    public GameObject portal;
     public int HP;
+    private int initHP = 500;
+
     // Start is called before the first frame update
     void Start()
     {
         HP = initHP;
-
     }
+
     // Update is called once per frame
     void Update()
     {
         if (HP <= 0)
         {
+            portal.SetActive(true);
             Destroy(gameObject);
-            
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Attack"))
         {
+
             Destroy(collision.gameObject);
-
-            HP = HP - 50;
-            
-
 
         }
     }
