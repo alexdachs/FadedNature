bb541a3f15483122842c665962fffb2069259449
david-ctrl <56065560+david-ctrl@users.noreply.github.com>
2020-05-14 20:27:17 +0200

    Muerte
    Muerte

---
+++ Assets/muerte.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muerte : MonoBehaviour
+{
+    private int initHP = 100;
+    public int HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = initHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Attack"))
+        {
+            Destroy(collision.gameObject);
+
+            HP = HP - 50;
+            
+
+
+        }
+    }
+}
