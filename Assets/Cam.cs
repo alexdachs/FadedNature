109a891e4f58f4003e2862e4e9a888a92d612cf6
alexdachs <56065187+alexdachs@users.noreply.github.com>
2020-05-14 17:13:51 +0200

    Revert "Merge branch 'master' of https://github.com/alexdachs/ejemplo.SUPERPROJECT"
    This reverts commit 65fd6dc5231d35e6e8ac74e8c1081f517752b2f4, reversing
    changes made to 8efc4f2f897ab7057d800d27abfcf024b8d8cb36.

--- Assets/Cam.cs
+++ Assets/Cam.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
-    public Transform Start, End;
     public GameObject follow;
     public int maxpos;
    
@@ -15,13 +14,5 @@ public class Cam : MonoBehaviour
         float posx = follow.transform.position.x;
 
         transform.position = new Vector3(Mathf.Clamp(posx, 0, maxpos), transform.position.y, transform.position.z);
-        if (transform.position.x <= Start.position.x)
-        {
-            transform.position = new Vector3(Start.position.x, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x >= End.position.x)
-        {
-            transform.position = new Vector3(End.position.x, transform.position.y, transform.position.z);
-        }
     }
 }
